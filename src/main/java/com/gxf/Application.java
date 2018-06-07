9bd8fa075cd426874776bfd4845b0f1b331a3fd8
luckygxf <403261934@qq.com>
2018-06-07 18:21:56 +0800

    debug

--- src/main/java/com/gxf/Application.java
+++ src/main/java/com/gxf/Application.java
@@ -1,26 +1,15 @@
 package com.gxf;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Configuration;
 
 
 /**
  * Created by GuanXF on 2018/3/5.
  */
-@Configuration
 @SpringBootApplication
-@EnableAutoConfiguration
-public class Application extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Application.class);
-    }
+public class Application {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Application.class, args);
--- src/main/java/com/gxf/websocket/WebSocketTest.java
+++ src/main/java/com/gxf/websocket/WebSocketTest.java
@@ -1,5 +1,6 @@
 package com.gxf.websocket;
 
+
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
@@ -8,7 +9,7 @@ import java.util.concurrent.CopyOnWriteArraySet;
 /**
  * Created by 58 on 2018/6/5.
  */
-@ServerEndpoint("/websocket")
+@ServerEndpoint("/clinet")
 public class WebSocketTest {
     //当前连接数
     private static int onlineCount = 0;
